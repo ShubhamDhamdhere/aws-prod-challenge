72ce996588b860df395de7c3655dfc0102b872d6
ShubhamDhamdhere <sid400604@gmail.com>
2026-05-05 14:09:03 +0000

    set

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
 output "alb_dns" {
-  value = module.alb.alb_dns
+  value = module.alb.alb_dns_name
+}
+
+output "asg_name" {
+  value = module.asg.asg_name
 }

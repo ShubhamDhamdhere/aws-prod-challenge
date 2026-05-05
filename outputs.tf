c8d540240cc32f8670e3c89a440d32cb9621e52b
ShubhamDhamdhere <sid400604@gmail.com>
2026-05-05 15:54:09 +0000

    infra

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
-output "alb_dns" {
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "alb_dns_name" {
   value = module.alb.alb_dns_name
 }
+
+output "asg_name" {
+  value = module.asg.asg_name
+}
+
